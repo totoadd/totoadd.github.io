1573ce36d6644d60e213b0fedb352c6907a96698
cos_smo <47656890+totoadd@users.noreply.github.com>
2020-10-12 21:19:59 +0200

    Update invite.js

--- invite.js
+++ invite.js
@@ -1,4 +1,8 @@
-var infos = $(location).attr('href').split('?')
-
-$("#text").html(`${infos[1]} t’as invité à ajouter Totoadd sur ton serveur.`)
-$('#pdp').attr('src', infos[2]);
+try {
+    var infos = decodeURI($(location).attr('href')).split('?')
+    $('#pdp').attr('src', infos[2]);
+    $("#text").html(`${infos[1]} t’as invité à ajouter Totoadd sur ton serveur.`)
+    document.title = `Totoadd - Invitation de ${infos[1]}`;  
+  }catch(e){ // catches a malformed URI
+    window.location.replace("https://totoadd.github.io");
+}
